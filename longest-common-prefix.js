864c5fe45feedaad8f93c02c151a8e3249e0c08e
UnknownMonk <smodcartoon@gmail.com>
2019-07-25 08:49:02 -0400

    leet code longest

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -1,17 +1,41 @@
+// var longestCommonPrefix = function(strs) {
+//   let prefix = [];
+//   strs.forEach(str => {
+//     prefix.push(str.slice(0, 2));
+//   });
+//   if (strs === []) {
+//     return '';
+//   }
+
+//   if (prefix[0] === prefix[1] && prefix[1] === prefix[2]) {
+//     return prefix[0];
+//   } else {
+//     return '';
+//   }
+// };
+
 var longestCommonPrefix = function(strs) {
-  let prefix = [];
-  strs.forEach(str => {
-    prefix.push(str.slice(0, 2));
-  });
-  if (strs === []) {
+  if (!strs || !strs.length) {
     return '';
+  }
+  if (strs.length == 1) {
+    return strs.find(() => true);
   }
 
-  if (prefix[0] === prefix[1] && prefix[1] === prefix[2]) {
-    return prefix[0];
-  } else {
-    return '';
+  strs.sort(function(a, b) {
+    return a.length - b.length;
+  });
+
+  let minLength = strs[0].length;
+  console.log(minLength);
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].slice(0, minLength) !== strs[0].slice(0, minLength)) {
+      minLength--;
+    }
+    if (minLength < 0) return '';
   }
+  return strs[0].slice(0, minLength);
 };
 
 //'itterate over the array'
